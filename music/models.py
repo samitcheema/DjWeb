529312062a1d939c23dd4a240ede01be1604625e
usamatcheema <usamatcheema@gmail.com>
2019-08-19 23:56:47 -0400

    added models

--- music/models.py
+++ music/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Albums(models.Model):
+    name = models.CharField(max_length=100)  # name of album
+    title = models.CharField(max_length=100)  # title of album
+    genre = models.CharField(max_length=50)  # genre of album
+    logo = models.CharField(max_length=1000)  # album logo
+    year = models.CharField(max_length=4)  # album year
+    record_label = models.CharField(max_length=100)  # record label
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Albums, on_delete=models.CASCADE)  # song album, on deletion delete all reference to song.
+    format = models.CharField(max_length=10)  # file type, i.e. mp4
+    song_title = models.CharField(max_length=100)  # song title
+    duration = models.CharField(max_length=100)  # song duration
