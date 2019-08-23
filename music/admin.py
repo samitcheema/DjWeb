c66826abddeee003d17a0c093dbd7ff501413fd5
usamatcheema <usamatcheema@gmail.com>
2019-08-22 22:10:24 -0400

    changes; added models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Albums
 
-# Register your models here.
+admin.site.register(Albums)
--- music/models.py
+++ music/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 
 class Albums(models.Model):
-    name = models.CharField(max_length=100)  # name of album
-    title = models.CharField(max_length=100)  # title of album
-    genre = models.CharField(max_length=50)  # genre of album
-    logo = models.CharField(max_length=1000)  # album logo
-    year = models.CharField(max_length=4)  # album year
-    record_label = models.CharField(max_length=100)  # record label
+    name = models.CharField(max_length=100, default='')  # name of artist
+    title = models.CharField(max_length=100, default='')  # title of album
+    genre = models.CharField(max_length=50, default='')  # genre of album
+    logo = models.CharField(max_length=1000, default='')  # album logo
+    year = models.CharField(max_length=4, default='')  # album year
+    record_label = models.CharField(max_length=100, default='')  # record label
+
+    def __str__(self):
+        return 'Artist: ' + self.name + ' Album title: ' + self.title
 
 
 class Song(models.Model):
     album = models.ForeignKey(Albums, on_delete=models.CASCADE)  # song album, on deletion delete all reference to song.
-    format = models.CharField(max_length=10)  # file type, i.e. mp4
-    song_title = models.CharField(max_length=100)  # song title
-    duration = models.CharField(max_length=100)  # song duration
+    format = models.CharField(max_length=10, default='')  # file type, i.e. mp4
+    song_title = models.CharField(max_length=100, default='')  # song title
+    duration = models.CharField(max_length=100, default='')  # song duration
